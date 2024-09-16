d4e89d0d50c00fa331612c9a9c9917d611539ea0
Martina at Home <20114816@tafe.wa.edu.au>
2024-09-16 13:04:21 +0800

    Test: modified check_winner to be suitable for testing

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ board = [' '] * 9
 current_player = 'X'
 
 # NOTE: you cannot use this answer in Portfolio Part 2
-def check_winner():
+def check_winner(board):
     """
     Check for a winner in the game.
     This function checks all possible winning combinations on the board.
@@ -64,7 +64,7 @@ def play(cell):
     global current_player
     if board[cell] == ' ':
         board[cell] = current_player
-        if not check_winner():
+        if not check_winner(board):
             current_player = 'O' if current_player == 'X' else 'X'
     return redirect(url_for('index'))
 
